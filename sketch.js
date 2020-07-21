7c26fb4b54638be89ae43333bf4cfaf93c72855b
saranpriya <priya.guna2012@gmail.com>
2020-07-21 18:26:27 +0530

    file change

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    platform = new Ground(300,100,1200,20);
+    platform = new Ground(300,300,1200,20);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
